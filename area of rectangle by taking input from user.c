38dac8cd7bc5d287a185e0c7642d24f3d43a74cd
Alish Rani <94797867+AlishRani@users.noreply.github.com>
2021-11-26 18:31:47 +0530

    Add files via upload

---
+++ area of rectangle by taking input from user.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+int main()
+{
+    float r;
+    float pi=3.14;
+    printf("enter the value of r");
+    scanf("%f",&r);
+    float a=pi*r*r;
+    printf("area of rectangle%d",a);
+    return 0;
+}
